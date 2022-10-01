153d3d95ff7553c2bb8bb6c54811a9daa8ccea5e
bhy04 <114802243+bhy04@users.noreply.github.com>
2022-10-01 20:18:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小兔兔',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oh5Z76Dp3eVqbt44Ri_uy9ZxYygI',
+      id: 'oh5Z76EMLeEtXxa91gVptWTvNCpQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'dHnfm6a5gP35GUD53ODDWMThli9phiao3dxaOscTgYk',
       festivals: [
